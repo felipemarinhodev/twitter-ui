790cd3f7100b29a2e391f739b38461ad2989ca30
Felipe Marinho <felipmarinho@gmail.com>
2020-08-24 11:24:15 -0300

    Creating central of icons of project

---
+++ src/styles/icons.ts
@@ -0,0 +1,12 @@
+export {
+  Home,
+  Email,
+  Search,
+  ExitToApp,
+  LocationOn,
+  Notifications,
+  FavoriteBorder,
+} from 'styled-icons/material-outlined';
+export { Person, Cake, Favorite } from 'styled-icons/material';
+export { ArrowLeft } from 'styled-icons/heroicons-solid';
+export { Chat } from 'styled-icons/heroicons-outline';
