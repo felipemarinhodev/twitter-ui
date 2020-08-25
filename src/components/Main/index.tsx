3c3302ecb066e034d8026cc6c4ee42a9a7ade702
Felipe Marinho <felipmarinho@gmail.com>
2020-08-25 05:23:32 -0300

    Importing icons for the bar

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
-import { Container, Header, BackIcon, ProfileInfo } from './styles';
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon,
+} from './styles';
 
 const Main: React.FC = () => {
   return (
@@ -17,12 +27,12 @@ const Main: React.FC = () => {
       </Header>
       {/* <ProfilePage /> */}
 
-      {/* <BottomManu>
-        <HomeIcon />
+      <BottomMenu>
+        <HomeIcon className="active" />
         <SearchIcon />
         <BellIcon />
         <EmailIcon />
-      </BottomManu> */}
+      </BottomMenu>
     </Container>
   );
 }
