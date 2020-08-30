d915033e30fbcf1b4ec25c819044fa41f87dcf09
Felipe Marinho <felipmarinho@gmail.com>
2020-08-30 17:57:59 -0300

    Creating Tweet component

---
+++ src/components/Tweet/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Tweet: React.FC = () => {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default Tweet;
