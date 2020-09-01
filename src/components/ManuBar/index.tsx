61650c443cdc9b53fdf0ae7438e6892a47d5ed4d
Felipe Marinho <felipmarinho@gmail.com>
2020-09-01 04:58:33 -0300

    Adding MenuBar Component

---
+++ src/components/ManuBar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ManuBar: React.FC = () => {
+  return (
+    <Container></Container>
+  );
+}
+
+export default ManuBar;
