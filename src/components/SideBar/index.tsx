a109f6766e5049fb95c8661bf62feab7992fda1b
Felipe Marinho <felipmarinho@gmail.com>
2020-09-01 21:17:27 -0300

    Improving Sidebar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
+import List from '../List';
 
 const SideBar: React.FC = () => {
   return (
     <Container>
-      
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <Body>
+        <List
+          title="Talvez você curta"
+          elements={[<h1>Test</h1>, <h1>Test</h1>, <h1>Test</h1>]}
+        />
+        <List
+          title="Talvez você curta"
+          elements={[<h1>Test</h1>, <h1>Test</h1>, <h1>Test</h1>]}
+        />
+      </Body>
     </Container>
   );
 }
