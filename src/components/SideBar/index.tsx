3cce9c5aa842df1c7e49bf4c1e32e83060da3300
Felipe Marinho <felipmarinho@gmail.com>
2020-09-01 21:34:13 -0300

    Adding FollowSuggestion component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -8,6 +8,7 @@ import {
   Body,
 } from './styles';
 import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
 
 const SideBar: React.FC = () => {
   return (
@@ -20,7 +21,20 @@ const SideBar: React.FC = () => {
       <Body>
         <List
           title="Talvez você curta"
-          elements={[<h1>Test</h1>, <h1>Test</h1>, <h1>Test</h1>]}
+          elements={[
+            <FollowSuggestion
+              name="Vivi Lima"
+              nickname="@vii829"
+            />,
+            <FollowSuggestion
+              name="Malu Lima"
+              nickname="@mllm"
+            />,
+            <FollowSuggestion
+              name="Felipe Barbosa"
+              nickname="@felipebarbosa"
+            />
+          ]}
         />
         <List
           title="Talvez você curta"
